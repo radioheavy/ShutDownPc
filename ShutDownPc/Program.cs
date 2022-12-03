eabfd9bcfe7a7a8b80811fc434203228d9e2e769
İsmail Oktay Dak <dakoktaytgb@gmail.com>
2022-12-03 12:26:50 +0300

    Add project files.

---
+++ ShutDownPc/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Shut_Down_Computer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bilgisayarı kapatmak istediğinize emin misiniz? (E/H)");
+            string response = Console.ReadLine().ToUpper();
+
+            if (response == "E")
+            {
+                System.Diagnostics.Process.Start("shutdown", "/s /t 0");
+                Console.WriteLine("Bilgisayar kapatılıyor...");
+            }
+            else if (response == "H")
+            {
+                Console.WriteLine("Kapatma iptal edildi.");
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz giriş. Lütfen tekrar deneyin.");
+            }
+        }
+    }
+}
